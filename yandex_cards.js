90d5d7e3aefed6811b052cc0372be9a9c198bc72
oOFaYOo <nika.tseleva@mail.ru>
2021-01-29 18:59:27 +0500

    Initial commit

---
+++ yandex_cards.js
@@ -0,0 +1,16 @@
+let from = document.getElementById("from");
+let to = document.getElementById("to");
+let about = document.getElementById("about");
+let add_path = document.getElementById("add_path");
+let added = document.getElementById("added");
+let final_path = document.getElementById("final_path");
+let get_path = document.getElementById("get_path");
+
+class OneDirection {
+    constructor(from, to, about){
+        this.from = from;
+        this.to = to;
+        this.about = about;
+    }
+}
+
