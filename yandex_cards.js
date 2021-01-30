70a6e82087c918e8b47dc925a04acc32c4eb021d
oOFaYOo <nika.tseleva@mail.ru>
2021-01-30 18:57:56 +0500

    Refactoring. Allocation of the module with the sorting function, optimization of the sorting function. Dynamic formation of the output line with the route.

--- yandex_cards.js
+++ yandex_cards.js
@@ -1,82 +1,81 @@
 // Использование только в Google Chrome.
-// Входные данные принимаются в виде строк.
-// Первая строка "город отправления",
-// вторая строка "город прибытия",
-// третья строка "подробное описание способа путешествия, транспорта, номера места и тд".
 
 "use strict";
 
+import {sortCards} from "./api_sortCards.js";
+
 const from = document.getElementById("from");
 const to = document.getElementById("to");
-const about = document.getElementById("about");
-const add_path = document.getElementById("add_path");
+const transport = document.getElementById("transport");
+const fromPlace = document.getElementById("from_place");
+const toPlace = document.getElementById("to_place");
+const numOfSeat = document.getElementById("num_of_seat");
+const addPath = document.getElementById("add_path");
 const added = document.getElementById("added");
-const final_path = document.getElementById("final_path");
-const get_path = document.getElementById("get_path");
+const finalPath = document.getElementById("final_path");
+const getPath = document.getElementById("get_path");
 
-let unsortedDirections = [];
+let unsortedCards = [];
 
-class OneDirection {
-    constructor(from, to, about) {
-        this.from = from;
-        this.to = to;
-        this.about = about;
+class Card {
+    constructor(from, to, fromPlace, toPlace, transport, numOfSeat) {
+        this.from = from.toLowerCase();
+        this.to = to.toLowerCase();
+        this.fromPlace = fromPlace;
+        this.toPlace = toPlace;
+        this.transport = transport;
+        this.numOfSeat = numOfSeat;
     }
 }
 
-add_path.onclick = () => {
-    if (from.value !== "" && to.value !== "" && about.value !== "") {
-        unsortedDirections.push(new OneDirection(from.value.toLowerCase(), to.value.toLowerCase(), about.value));
-        added.innerHTML = added.innerHTML + `<p>- ${about.value}</p>`;
+addPath.onclick = () => {
+    if (from.value !== "" &&
+        to.value !== "" &&
+        transport.value !== "" &&
+        fromPlace.value !== "" &&
+        toPlace.value !== "" &&
+        numOfSeat.value !== "") {
+        unsortedCards.push(new Card(from.value, to.value, fromPlace.value, toPlace.value, transport.value, numOfSeat.value));
+        added.innerHTML = added.innerHTML + `<p>- Начало пути в городе ${formatWordToSentenceStandard(from.value)}. Отправление из ${fromPlace.value}. Используемый транспорт ${transport.value.toLowerCase()}. Сиденье №${numOfSeat.value}. Прибытие в ${toPlace.value} города ${formatWordToSentenceStandard(to.value)}.</p>`;
         from.value = "";
         to.value = "";
-        about.value = "";
+        fromPlace.value = "";
+        toPlace.value = "";
+        transport.value = "";
+        numOfSeat.value = "";
     } else {
-        alert("Пожалуйста, заполните пустые поля");
-        console.log("Пожалуйста, заполните пустые поля");
+        let errorMessage = "Пожалуйста, заполните пустые поля";
+        alert(errorMessage);
+        console.log(errorMessage);
     }
 };
 
-get_path.onclick = () => {
-    if(unsortedDirections.length === 0 && from.value !== "" && to.value !== "" && about.value !== ""){
-        final_path.innerHTML = final_path.innerHTML + `<p>${about.value}</p>`;
+getPath.onclick = () => {
+    finalPath.innerHTML = "";
+    if(unsortedCards.length === 0 &&
+        from.value !== "" &&
+        to.value !== "" &&
+        transport.value !== "" &&
+        fromPlace.value !== "" &&
+        toPlace.value !== "" &&
+        numOfSeat.value !== ""){
+        finalPath.innerHTML = finalPath.innerHTML + `<p>- Начало пути в городе ${formatWordToSentenceStandard(from.value)}. Отправление из ${fromPlace.value}. Используемый транспорт ${transport.value.toLowerCase()}. Сиденье №${numOfSeat.value}. Прибытие в ${toPlace.value} города ${formatWordToSentenceStandard(to.value)}.</p>`;
     }
-    let sortedDirections = sorting(unsortedDirections);
+    let sortedCards = sortCards(unsortedCards);
     from.value = "";
     to.value = "";
-    about.value = "";
+    fromPlace.value = "";
+    toPlace.value = "";
+    transport.value = "";
+    numOfSeat.value = "";
     added.innerHTML = "";
-    for (let val of sortedDirections) {
-        final_path.innerHTML = final_path.innerHTML + `<p>${val.about}</p>`;
+    for (let card of sortedCards) {
+        finalPath.innerHTML = finalPath.innerHTML + `<p>- Начало пути в городе ${formatWordToSentenceStandard(card.from)}. Отправление из ${card.fromPlace}. Используемый транспорт ${card.transport.toLowerCase()}. Сиденье №${card.numOfSeat}. Прибытие в ${card.toPlace} города ${formatWordToSentenceStandard(card.to)}.</p>`;
     }
+    sortedCards = [];
 };
 
-function sorting(arr) {
-    let unsortedArr = arr.slice();
-    let resultArr = [];
-// find first element of result arr
-    for (let i = 0; i < unsortedArr.length; i++) {
-        let counter = 0;
-        for (let j = 0; j < unsortedArr.length; j++) {
-            if (unsortedArr[i].from === unsortedArr[j].to) {
-                counter = counter + 1;
-            }
-        }
-        if (counter === 0) {
-            resultArr.push(unsortedArr[i]);
-            unsortedArr[i] = null;
-            break;
-        }
-    }
-    unsortedArr = unsortedArr.filter(item => item !== null);
-//---------------------------------
-    for (let i = 0; i < resultArr.length; i++) {
-        for (let j = 0; j < unsortedArr.length; j++) {
-            if (resultArr[i].to === unsortedArr[j].from) {
-                resultArr.push(unsortedArr[j]);
-            }
-        }
-    }
-    return resultArr;
+function formatWordToSentenceStandard(string) {
+    return string[0].toUpperCase()+string.slice(1);
 }
 
